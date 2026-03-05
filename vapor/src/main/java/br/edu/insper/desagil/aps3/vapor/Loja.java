cb5e369142c04a1f1976095e36ecfbda53a9c2ad
Benclo <riquebenclo@gmail.com>
2026-03-05 10:47:51 -0300

    feat: tarefa 2 - Loja

--- vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Loja.java
+++ vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Loja.java
@@ -1,4 +1,29 @@
 package br.edu.insper.desagil.aps3.vapor;
 
+import java.util.List;
+
 public class Loja {
+
+    private Usuario dono;
+    private List<Jogo> jogos;
+
+    public Loja(Usuario dono, List<Jogo> jogos){
+        this.dono = dono;
+        this.jogos = jogos;
+    }
+
+    public int avaliados(Usuario usuario){
+        String apelido = usuario.getApelido();
+        int soma = 0;
+        for (Jogo j : jogos){
+            if (j.isInAvaliacoes(apelido)){
+                soma += 1;
+            }
+        }
+        return soma;
+    }
+
+    public Usuario getDono() {
+        return dono;
+    }
 }
