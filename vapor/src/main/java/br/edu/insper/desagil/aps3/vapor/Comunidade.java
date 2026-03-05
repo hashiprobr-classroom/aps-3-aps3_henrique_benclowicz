986942229de9a650e8ffbee3c38cfc2b1b1bad98
Benclo <riquebenclo@gmail.com>
2026-03-05 11:08:11 -0300

    feat: tarefa 2 - comunidade

--- vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Comunidade.java
+++ vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Comunidade.java
@@ -1,4 +1,14 @@
 package br.edu.insper.desagil.aps3.vapor;
 
+import java.util.List;
+
 public class Comunidade {
+    private List<Usuario> usuarios;
+
+    public Comunidade(List<Usuario> usuarios){
+        this.usuarios = usuarios;
+    }
+    public void addUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
 }
