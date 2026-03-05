f8d7ba87e0a61472350626a676db6e3e19734208
Benclo <riquebenclo@gmail.com>
2026-03-05 13:07:27 -0300

    feat: tarefa 2 - completa

--- vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Comunidade.java
+++ vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Comunidade.java
@@ -2,8 +2,8 @@ package br.edu.insper.desagil.aps3.vapor;
 
 import java.util.List;
 
-public class Comunidade {
-    private List<Usuario> usuarios;
+public abstract class Comunidade {
+    public List<Usuario> usuarios;
 
     public Comunidade(List<Usuario> usuarios){
         this.usuarios = usuarios;
--- vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/ComunidadeDeJogo.java
+++ vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/ComunidadeDeJogo.java
@@ -1,4 +1,18 @@
 package br.edu.insper.desagil.aps3.vapor;
 
-public class ComunidadeDeJogo {
+import java.util.List;
+
+public class ComunidadeDeJogo extends Comunidade{
+    private Jogo jogo;
+    public ComunidadeDeJogo(Jogo jogo, List<Usuario> usuarios){
+        super(usuarios);
+
+    }
+
+    @Override
+    public void addUsuario(Usuario usuario){
+        if (jogo.isInAvaliacoes(usuario.getApelido())){
+            usuarios.add(usuario);
+        }
+    }
 }
